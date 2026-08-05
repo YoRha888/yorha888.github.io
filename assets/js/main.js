4a39504d4dcaf87517b7c63345b24d7d80c60008
FLOAT WORLD <Cloud.0907@icloud.com>
2026-08-06 01:41:18 +0900

    Delete assets/js directory

--- assets/js/main.js
+++
@@ -1,4 +0,0 @@
-
-const toggle=document.querySelector('.menu-toggle');const nav=document.querySelector('.nav');
-if(toggle&&nav){toggle.addEventListener('click',()=>nav.classList.toggle('is-open'));}
-document.querySelectorAll('.year').forEach(el=>{el.textContent=new Date().getFullYear();});
